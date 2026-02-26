77453f2c7df60b48d6a150537c839e5a8f835e83
Miya Livingston <miyaolivingston@gmail.com>
2026-02-26 00:04:08 -0500

    Removed unused dependencies

--- src/run_baseline.py
+++ src/run_baseline.py
@@ -8,10 +8,9 @@ import contextlib
 from tqdm import tqdm
 from itertools import product
 from datasets import load_dataset
-from openai import OpenAI
-from google import genai
 from config import TASKS, DIALECTS, MODELS, N, SEED, OUT_DIR, LOG_DIR
 from dotenv import load_dotenv
+from calls import call_model
 load_dotenv() # Load environment variables from .env (API keys)
 from huggingface_hub import login
 
@@ -281,37 +280,6 @@ def score(task, pred, label, example):
         return pred == label
    
     return False
-
-
-################################## Calling models ##################################
-def call_openai(prompt, model, retries=3, backoff_factor=2):
-    client = OpenAI()
-    for attempt in range(retries):
-        try:
-            resp = client.chat.completions.create(
-                model=model,
-                messages=[{"role": "user", "content": prompt}],
-                temperature=0,
-            )
-            return resp.choices[0].message.content.strip()
-        except Exception as e:
-            wait_time = backoff_factor ** attempt
-            print(f"API error: {e}. Retrying in {wait_time} seconds...")
-            time.sleep(wait_time)
-    raise Exception("Maximum retries exceeded.")
-
-def call_gemini(prompt, model):
-    client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
-    response = client.models.generate_content(
-        model=model,
-        contents=prompt
-    )
-    return response.text
-
-def call_model(prompt, model):
-    if model.startswith("gpt-"):
-        return call_openai(prompt, model)
-    return call_gemini(prompt, model)
 
 
 ################################## Output and logging directories ##################################
